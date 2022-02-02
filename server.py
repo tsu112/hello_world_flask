f641d472b5a1d66526807dec37aab5dfb28757fd
Sue Yang <sueyang112@hotmail.com>
2022-02-01 21:12:39 -0500

    Added the rendering exercise portion

--- server.py
+++ server.py
@@ -1,12 +1,11 @@
-from flask import Flask  # Import Flask to allow us to create our app
-# Create a new instance of the Flask class called "app"
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
-# The "@" decorator associates this route with the function immediately following
 @app.route('/')
-def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
+def index():
+    return render_template("index.html")
 
 
 @app.route('/success')
@@ -19,5 +18,5 @@ def hello(banana, num):
     return f"Hello {banana * num}"
 
 
-if __name__ == "__main__":   # Ensure this file is being run directly and not from a different module
-    app.run(debug=True)    # Run the app in debug mode.
+if __name__ == "__main__":
+    app.run(debug=True)
